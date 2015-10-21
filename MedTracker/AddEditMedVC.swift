94d2675e2b13b58913fd5a9496856f0064abbf8b
Justin Ferre <Jman4647@gmail.com>
2015-10-21 13:57:02 -0400

    added view controllers and images

---
+++ MedTracker/AddEditMedVC.swift
@@ -0,0 +1,35 @@
+//
+//  AddEditMedVC.swift
+//  MedTracker
+//
+//  Created by Justin Ferre on 10/21/15.
+//  Copyright © 2015 Justin Ferre. All rights reserved.
+//
+
+import UIKit
+
+class AddEditMedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
